78b204668de08af2faf0542fad9ea4392d779d47
Ruben <rub3n457@gmail.com>
2026-04-29 17:58:40 -0600

    solucion empleado obligatorio

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -14,7 +14,7 @@ class DashboardController extends Controller
     public function index()
     {
         $user = auth()->user();
-
+        
         if ($user->cliente) {
         return redirect()->route('clientes.index');
         }
