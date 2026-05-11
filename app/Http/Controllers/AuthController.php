61f94c77398aaadbb901b4b7d39ef5b71286f9b5
Yuriel rzr <rub3n457@gmail.com>
2026-05-11 13:37:18 -0600

    prueba3

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -23,12 +23,19 @@ class AuthController extends Controller
         ]);
 
         if (Auth::attempt($credentials)) {
-            $request->session()->regenerate();
-            if (auth()->user()->cliente) {
-            return redirect('/cliente');
-        }
 
-        return redirect('/'); // admin
+            $request->session()->regenerate();
+        
+
+            $user = auth()->user();
+
+            if ($user->rol_id == 2 && $user->cliente) {
+                return redirect('/cliente');
+            }
+
+            if ($user->rol_id == 1) {
+                return redirect('/');
+            }
         }
 
         return back()->withErrors([
